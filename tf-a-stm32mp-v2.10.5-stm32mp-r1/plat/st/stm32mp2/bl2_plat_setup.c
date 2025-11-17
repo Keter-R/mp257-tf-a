3f577f5c77f5a3173dbfc0076ecfedeaff9f622e
Keter-R <85301117+Keter-R@users.noreply.github.com>
2025-11-17 10:40:36 +0800

    Enable additional bit for Region 1 in CR

--- tf-a-stm32mp-v2.10.5-stm32mp-r1/plat/st/stm32mp2/bl2_plat_setup.c
+++ tf-a-stm32mp-v2.10.5-stm32mp-r1/plat/st/stm32mp2/bl2_plat_setup.c
@@ -467,7 +467,7 @@ void bl2_el3_plat_arch_setup(void)
     mmio_write_32(RISAB4_BASE + RISAB_ACR1_OFFSET, acr_val);  //  使用 RISAB4_BASE
 
     /* Enable Region 1 in the Control Register (CR) */
-    mmio_setbits_32(RISAB4_BASE + RISAB_CR, BIT(1)); // 使用 RISAB4_BASE
+    mmio_setbits_32(RISAB4_BASE + RISAB_CR, BIT(1) | BIT(17)); // 使用 RISAB4_BASE
 
     INFO("RISAB4: Configured Region 1 for shared memory @ 0x%lx\n", SHARED_MEM_BASE_ADDR);
 }
