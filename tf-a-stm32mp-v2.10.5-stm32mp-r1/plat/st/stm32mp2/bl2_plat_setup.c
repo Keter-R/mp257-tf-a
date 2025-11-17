bd21cde1fef2062812d374528d7f3ae1d5171661
Keter-R <85301117+Keter-R@users.noreply.github.com>
2025-11-17 10:56:37 +0800

    Add warning for M-CORE shared memory status

--- tf-a-stm32mp-v2.10.5-stm32mp-r1/plat/st/stm32mp2/bl2_plat_setup.c
+++ tf-a-stm32mp-v2.10.5-stm32mp-r1/plat/st/stm32mp2/bl2_plat_setup.c
@@ -470,6 +470,7 @@ void bl2_el3_plat_arch_setup(void)
     mmio_setbits_32(RISAB4_BASE + RISAB_CR, BIT(1) | BIT(17)); // 使用 RISAB4_BASE
 
     INFO("RISAB4: Configured Region 1 for shared memory @ 0x%lx\n", SHARED_MEM_BASE_ADDR);
+    WARN("TF-A BOOT CHECK: *** M-CORE SHARED MEMORY IS RUNNING ***\n");
 }
 #endif
 /*****************************************************************/
